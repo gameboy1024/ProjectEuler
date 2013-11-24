ff648d9e0a457370344f35db0cebb33db88549e6
Botu Sun <gameboy1024@hotmail.com>
2013-11-24 23:21:07 +0100

    Problems 1-10
    Problems from 1 to 10. First Commit.

---
+++ src/problems/Problem3.java
@@ -0,0 +1,48 @@
+package problems;
+/**
+ * Largest prime factor
+ * Problem 3
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * 
+ * What is the largest prime factor of the number 600851475143 ?
+ * 
+ * 
+ * Answer:
+ * 6857
+ * Completed on Sun, 24 Nov 2013, 16:27
+ * http://projecteuler.net/problem=3
+ * 
+ * @author SBT
+ *
+ */
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+		long number = 600851475143l;
+		long middle = (int) Math.sqrt(number);
+		long i;
+		long factor = 0;
+		// handle even numbers:
+		if (number % 2 == 0) {
+			factor = 2;
+			while (number % 2 == 0) {
+				number >>= 1;
+			}
+		}
+		for (i = 3; i <= middle; i += 2) {
+			if (number % i == 0) {
+				number = number / i;
+				factor = i;
+				while (number % i == 0) {
+					number = number / i;
+				}
+			}
+		}
+		if (factor == 0) {
+			System.out.println("This is a prime number, no answer!");
+		} else {
+			System.out.println("Answer is : " + factor);
+		}
+	}
+}
---
+++ src/problems/Problem7.java
@@ -0,0 +1,45 @@
+package problems;
+/**
+ * 10001st prime
+ * Problem 7
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ * 
+ * What is the 10 001st prime number?
+ * 
+ * 
+ * Answer:
+ * 104743
+ * Completed on Sun, 24 Nov 2013, 19:16
+ * http://projecteuler.net/problem=7
+ * 
+ * @author SBT
+ *
+ */
+public class Problem7 {
+
+	public static void main(String[] args) {
+		long prime[] = new long[10001];
+		boolean found = false;
+		prime[0] = 2;
+		prime[1] = 3;
+		int count = 1;
+		while (count < 10000) {
+			label: for (long i = prime[count] + 1;; i++) {
+				found = true;
+				for (int j = 0; j <= count; j++) {
+					if (i % prime[j] == 0) {
+						found = false;
+					}
+				}
+				if (found) {
+					// System.out.println("Found prime : " + i);
+					prime[++count] = i;
+					break label;
+				}
+			}
+		}
+		System.out.println("Answer is : " + prime[10000]);
+
+	}
+
+}
