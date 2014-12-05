5e61ecb631c0380ad21e133f26a017f312d37db3
SunBotu <gameboy1024@hotmail.com>
2014-12-05 13:28:32 +0100

    Solution to problem 74 & modification to 73

--- src/problem_73.py
+++ src/problem_73.py
@@ -2,6 +2,7 @@
 '''
   Counting fractions in a range
   Problem 73
+  
   Consider the fraction, n/d, where n and d are positive integers. If n<d and 
   HCF(n,d)=1, it is called a reduced proper fraction.
 
---
+++ src/problem_74.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+'''
+  Digit factorial chains
+  Problem 74
+  The number 145 is well known for the property that the sum of the factorial of
+  its digits is equal to 145:
+
+  1! + 4! + 5! = 1 + 24 + 120 = 145
+
+  Perhaps less well known is 169, in that it produces the longest chain of 
+  numbers that link back to 169; it turns out that there are only three such 
+  loops that exist:
+
+  169 → 363601 → 1454 → 169
+  871 → 45361 → 871
+  872 → 45362 → 872
+
+  It is not difficult to prove that EVERY starting number will eventually get 
+  stuck in a loop. For example,
+
+  69 → 363600 → 1454 → 169 → 363601 (→ 1454)
+  78 → 45360 → 871 → 45361 (→ 871)
+  540 → 145 (→ 145)
+
+  Starting with 69 produces a chain of five non-repeating terms, but the longest
+  non-repeating chain with a starting number below one million is sixty terms.
+
+  How many chains, with a starting number below one million, contain exactly 
+  sixty non-repeating terms?
+
+  Answer: 402 Completed on Thu, 4 Dec 2014, 10:33
+  https://projecteuler.net/problem=74
+  
+  @author Botu Sun
+'''
+
+import math
+
+factorials = [1] * 10
+for i in xrange(1, 10):
+  factorials[i] = factorials[i - 1] * i
+print factorials
+
+loopers = {145: 0, 169: 2, 871: 1, 872: 1, 40585: 0} # 40585 is also a looper.
+cache = {}
+
+count = 0
+for i in xrange(3, 1000000):
+  chain = 1
+  numbers = []
+  while i not in loopers:
+    if i in cache:
+      chain += cache[i]
+      break
+    numbers.append(i)
+    if i == 2: break
+    total = 0
+    while i:
+      total += factorials[i % 10]
+      i /= 10
+    i = total
+    chain += 1
+  chain += loopers.get(i, 0)
+  if chain == 60:
+    count += 1
+    
+  while True:
+    n = numbers.pop()
+    if n not in cache:
+      cache[n] = chain
+  
+  
+print count
