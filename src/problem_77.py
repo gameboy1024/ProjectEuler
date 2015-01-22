9747ad5ee98a18ed45982ac53f2fe94d943d87da
gameboy1024 <gameboy1024@hotmail.com>
2015-01-22 12:01:19 +0100

    Solution to 77w

--- src/problem_77.py
+++ src/problem_77.py
@@ -14,7 +14,7 @@
   What is the first value which can be written as the sum of primes in over five
   thousand different ways?
 
-  Answer: 161667 Completed on Mon, 5 Jan 2015, 23:47
+  Answer: 71 Completed on Thu, 22 Jan 2015, 11:59
   https://projecteuler.net/problem=77
   
   @author Botu Sun
@@ -23,37 +23,25 @@
 from lib.math_utils import PrimeChecker
 
 prime_checker = PrimeChecker(1E5)
-def Function(x, y, map, prime_checker, t):
-  print t, x, y
-  raw_input()
-  # if x == 1 or y == 1: return 0
+def Function(x, y, map, prime_checker):
+  if x == 0: return 1
   if x < 2: return 0
-  if x == 2: 
-    print 'return', 1
-    return 1
   if (x, y) in map: return map[(x, y)]
   sum = 0
   for p in prime_checker.get_prime_list():
     if p <= x and p <= y:
-      # print t, p
-      tmp = Function(x - p, p, map, prime_checker, t + '\t')
+      tmp = Function(x - p, p, map, prime_checker)
       sum += tmp
-      print 'sum', sum
     else:
       break
   map[(x, y)] = sum
-  print 'return', sum
   return sum
 
 map = {}
 
-print Function(10, 9, map, prime_checker, '')
-print map
-exit()
-
 i = 1
-while (Function(i, i - 1, map, prime_checker) <= 5):
+while (Function(i, i - 1, map, prime_checker) <= 5000):
   i += 1
 
 print i
-  
+
